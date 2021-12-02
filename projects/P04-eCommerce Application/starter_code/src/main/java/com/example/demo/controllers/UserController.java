ea57a68a7b1e3207a252158170870d58b6521cde
Brian Lee <brianclee7@deloitte.com>
2021-12-02 09:16:40 -0700

    I have updated files to fix issues.

--- projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
+++ projects/P04-eCommerce Application/starter_code/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,12 +1,8 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-import java.util.logging.LogManager;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
--- projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/security/SecurityTest.java
+++ projects/P04-eCommerce Application/starter_code/src/test/java/com/example/demo/security/SecurityTest.java
@@ -9,7 +9,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.junit.Assert.assertNotNull;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
